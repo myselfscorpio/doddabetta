70dd25c7e98bffd331b528c6e4a86612b81808cd
myselfscorpio <myself.scorpio@gmail.com>
2015-01-25 18:28:40 +0530

    first commit

---
+++ MavenWebDriverDemo/src/test/java/GoogleTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+/**
+ * @author Gaurang_Shah
+ */
+public class GoogleTest {
+	private WebDriver driver;
+
+	@Test
+	public void verifySearch() throws InterruptedException {
+		driver = new FirefoxDriver();
+		driver.get("http://www.google.com/");
+		Thread.sleep(16000);
+		driver.quit();
+		
+	}
+}
